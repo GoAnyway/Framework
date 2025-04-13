28e0d50a88f88818c045e4fc2ce44626ba57e83b
GoAnyway <woolftv@mail.ru>
2025-04-13 14:09:14 +0400

    Add tests.

---
+++ src/test/GoAnyway.Framework.Tests/DependencyInjection/Helpers/NonAttributedService.cs
@@ -0,0 +1,6 @@
+﻿namespace GoAnyway.Framework.Tests.DependencyInjection.Helpers;
+
+internal class NonAttributedService : ITestService
+{
+    public string GetValue() => "should not be registered";
+}
